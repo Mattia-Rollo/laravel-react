5cc033ffd1668572b87033d6ff0af7e4e6984506
Mattia-Rollo <mattiarollo89@gmail.com>
2023-04-13 16:15:24 +0200

    add show view product

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -58,6 +58,7 @@ class ProductController extends Controller
     public function show(Product $product)
     {
         //
+        return Inertia::render('Products/Show', compact('product'));
     }
 
     /**
--- resources/js/Pages/Products/Show.jsx
+++ resources/js/Pages/Products/Show.jsx
@@ -3,31 +3,30 @@ import { Head } from '@inertiajs/react';
 import PrimaryButton from '@/Components/PrimaryButton';
 import Form from '@/Components/Forms';
 import { useState } from 'react';
-import { Link } from '@inertiajs/react';
 
-export default function Products(props) {
-    const [active,setActive] = useState(false);
+export default function Product(props) {
+    // const [active,setActive] = useState(false);
 
-    function toggle(){
-        setActive(!active);
-    }
+    // function toggle(){
+    //     setActive(!active);
+    // }
     return (
         <AuthenticatedLayout
             auth={props.auth}
             errors={props.errors}
             header={<h2 className="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">Dashboard</h2>}
         >
-            <Head title="Products" />
+            <Head title="Product" />
 
             <div className="py-12">
                 <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
                     <div className="p-6 bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
-                        <h1 className=" text-gray-900 dark:text-gray-100 text-3xl pb-4">Products</h1>
+                        <h1 className=" text-gray-900 dark:text-gray-100 text-3xl pb-4">Product</h1>
                         <ul className='pb-6 text-gray-900 dark:text-gray-100'>
                             
-                        {props.products.map(product => (
-                            <li key={product.id}>{product.name} | {product.slug} | {product.description}</li>
-                            ) )}
+                        
+                            <li key={props.product.id}>{props.product.name} | {props.product.slug} | {props.product.description}</li>
+                            
 
                         </ul>
                         <table className="w-full text-gray-900 table-fixed w-100 rounded-xl dark:text-gray-300 border-collapse border border-slate-900 ">
@@ -39,24 +38,24 @@ export default function Products(props) {
                                 </tr>
                             </thead>
                             <tbody>
-                                {props.products.map(product => (
-                                    <tr key={product.id} className="text-gray-400">
-                                    <td className='border border-slate-700 p-5 text-center'>{product.name}</td>
-                                    <td className='border border-slate-700 p-5 text-center'><Link href={route('products.show', product)}>{product.slug}</Link></td>
-                                    <td className='border border-slate-700 p-5'>{product.description}</td>
+                                {
+                                    <tr key={props.product.id} className="text-gray-400">
+                                    <td className='border border-slate-700 p-5 text-center'>{props.product.name}</td>
+                                    <td className='border border-slate-700 p-5 text-center'>{props.product.slug}</td>
+                                    <td className='border border-slate-700 p-5'>{props.product.description}</td>
                                     </tr>
 
-                                ))}
+                                }
                                 
                             </tbody>
                         </table>
 
                         <div className='my-5'>
-                        <PrimaryButton action={toggle}>Add</PrimaryButton>
+                        {/* <PrimaryButton action={toggle}>Add</PrimaryButton> */}
 
                         </div>
-                        {active && <Form />
-                        }
+                        {/* {active && <Form />
+                        } */}
                     </div>
                 </div>
             </div>
